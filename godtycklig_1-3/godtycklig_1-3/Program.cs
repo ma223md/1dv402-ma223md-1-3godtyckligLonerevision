4caeec07d88634681fc50e5a396316a6e06aac02
ma223md <ma223md@student.lnu.se>
2014-09-19 16:03:12 +0200

    Fixed formating, and added try catch for salaries.

--- godtycklig_1-3/godtycklig_1-3/Program.cs
+++ godtycklig_1-3/godtycklig_1-3/Program.cs
@@ -14,8 +14,21 @@ namespace godtycklig_1_3
             int numberOfSalaries;
 
             // Kalla metoden ReadInt för att få antal löner
-            numberOfSalaries = ReadInt("Mata in antal löner: ");
-
+            while (true)
+            {
+                try
+                {
+                    numberOfSalaries = ReadInt("Mata in antal löner: "); 
+                    break;
+                } 
+                catch(FormatException) // Felmeddelande om något annat än ett heltal matas in
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Antalet måste vara ett heltal!");
+                    Console.ResetColor();
+                }
+            }
+            
             // Felmeddelande om inmatat antal är mindre än 2
             if (numberOfSalaries < 2)
             {
@@ -39,28 +52,56 @@ namespace godtycklig_1_3
             double avarageSalary, salarySpread;
 
             // Låt användaren skriva in lönerna
-            for (n = 1, i = 0; n <= numberOfSalaries; n++)
+            while (true)
             {
-                Console.Write("Lön {0}: ", n);
-                salaries[i] = int.Parse(Console.ReadLine());
-                i++;
+                try
+                {
+                    for (n = 1, i = 0; n <= numberOfSalaries; n++)
+                    {
+                        Console.Write("Ange lön {0}: ", n);
+                        salaries[i] = int.Parse(Console.ReadLine());
+                        i++;
+                    }
+                    break;
+                }
+                // Felmeddelande om annat än heltal anges
+                catch (FormatException)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Antalet måste vara ett heltal!");
+                    Console.ResetColor();
+                    return;
+                }
+
             }
 
+            Console.WriteLine("-------------------------------");
             // Räkna ut medellön
-            avarageSalary = salaries.Average();
-            Console.WriteLine("Medellön: {0}", avarageSalary);
+            avarageSalary = Math.Round(salaries.Average());
+            Console.WriteLine("Medellön: {0, 20:c0}", avarageSalary);
 
             // Räkna ut lönespridning
             salarySpread = salaries.Max() - salaries.Min();
-            Console.WriteLine("Lönespridning: {0}", salarySpread);
+            Console.WriteLine("Lönespridning: {0, 15:c0}", salarySpread);
 
             // Gör kopia på array för att räkna ut median
             Array.Copy(salaries, median, numberOfSalaries);
             Array.Sort(median);
-            Console.WriteLine("Medianlön: {0}", median[median.Length/2]);
-            Console.WriteLine(median[0]);
-            Console.WriteLine(median[1]);
-            Console.WriteLine(median[2]);
+            Console.WriteLine("Medianlön:{0, 20:c}", median[median.Length / 2]);
+            Console.WriteLine("-------------------------------");
+
+            // Skriv ut värdena i samma ordning som de skrevs in
+            for (i = 0, n = 0; i < numberOfSalaries; i++)
+            {
+                //Console.Write(salaries[i] + " ");
+                Console.Write("{0, 7}", salaries[i]);
+                n++;
+                if ( n > 2 && n % 3 == 0)
+                {
+                    Console.WriteLine("");
+                }
+            }
+            Console.WriteLine("");
 
         }
 
